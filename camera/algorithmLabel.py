f62b24847a81ef66e6fa8388c59b708b124a1c20
Bryan Luu <bryanluu30794@gmail.com>
2016-11-25 14:22:09 -0800

    Changed the output of the algorithmLabel to use an HTML formatted table.

--- camera/algorithmLabel.py
+++ camera/algorithmLabel.py
@@ -86,13 +86,13 @@ class AlgorithmLabel(QWidget):
 
     @pyqtSlot(int, int ,int ,int, int)
     def displayData(self, x, y, w, h, area):
-        displayText = ""
+        displayText = "<table border=1>"
 
-        displayText += "<b>X:</b>" + str(x) + "\n"
-        displayText += "<b>Y:</b>" + str(y) + "\n"
-        displayText += "<b>W:</b>" + str(w) + "\n"
-        displayText += "<b>H:</b>" + str(h) + "\n"
-        displayText += "<b>Area:</b>" + str(area) + "\n"
+        displayText += "<tr><td><b>x:</b></td><td>" + str(x) + "</td></tr>"
+        displayText += "<tr><td><b>y:</b></td><td>" + str(y) + "</td></tr>"
+        displayText += "<tr><td><b>w:</b></td><td>" + str(w) + "</td></tr>"
+        displayText += "<tr><td><b>h:</b></td><td>" + str(h) + "</td></tr>"
+        displayText += "<tr><td><b>Area:</b></td><td>" + str(area) + "</td></tr>"
 
         if len(self.x) < self.MEAN_LEN:
             self.x.append(x)
@@ -109,19 +109,20 @@ class AlgorithmLabel(QWidget):
         meanX = sum(self.x)/len(self.x)
         meanY = sum(self.y)/len(self.y)
 
-        displayText += "<b>Mean X:</b>" + str(meanX) + "\n"
-        displayText += "<b>Mean Y:</b>" + str(meanY) + "\n"
+        displayText += "<tr><td><b>Mean x:</b></td><td>" + str(meanX) + "</td></tr>"
+        displayText += "<tr><td><b>Mean y:</b></td><td>" + str(meanY) + "</td></tr>"
 
         try:
             Ypos = ((self.Y2box.value() - self.Y1box.value()) / (self.x2box.value() - self.x1box.value())) * (meanX - self.x1box.value()) + self.Y1box.value()
 
-            displayText += "<b>Y-Pos:</b>" + str(Ypos) + "\n"
+            displayText += "<tr><td><b>Real Y-Pos:</b></td><td>" + str(Ypos) + "</td></tr>"
 
             Zpos = ((self.Z2box.value() - self.Z1box.value()) / (self.y2box.value() - self.y1box.value())) * (meanY - self.y1box.value()) + self.Z1box.value()
 
-            displayText += "<b>Z-Pos:</b>" + str(Zpos) + "\n"
+            displayText += "<tr><td><b>Real Z-Pos:</b></td><td>" + str(Zpos) + "</td></tr>"
         except ZeroDivisionError as e:
             self.erroredOut.emit("Invalid calibration coordinates. Please check calibration coordinates. x, y corresponds to pixels, Y, Z corresponds to real dimensions.", ErrorPriority.Warning)
 
+        displayText += "</table>"
 
         self.text.setText(displayText)
