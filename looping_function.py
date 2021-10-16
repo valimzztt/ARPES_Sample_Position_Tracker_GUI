f2b62aaa237feeb3305c57daa49965ccc92abba4
valmzztt.stu <valmzztt@student.ubc.ca>
2021-10-16 14:18:26 -0700

    Cleaned up code in the camera package

--- looping_function.py
+++
@@ -1,8 +0,0 @@
-
-from algorithmchoice import WidgetGallery2
-
-def random_function(self, monitoring_time, gui_widget):
-    for i in range(monitoring_time):
-        print(gui_widget.createBottomLeftTabWidget(self))
-
-random_function(10, WidgetGallery2)
